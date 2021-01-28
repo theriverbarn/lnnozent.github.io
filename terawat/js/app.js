051153d53aeea5fd5545d20d93a57cf4b7bd247a
Sarawut <66264313+lnnozent@users.noreply.github.com>
2021-01-28 23:29:52 +0700

    Create app.js

---
+++ terawat/js/app.js
@@ -0,0 +1,7 @@
+var config = {
+  apiKey: "AIzaSyDVAMzBCavz4SZED2xU8lx-qpGgoyacIcM",
+  authDomain: "rasberry-boy.firebaseapp.com",
+  databaseURL: "https://rasberry-boy.firebaseio.com",
+  storageBucket: "rasberry-boy.appspot.com",
+  messagingSenderId: "664102215589"
+};
